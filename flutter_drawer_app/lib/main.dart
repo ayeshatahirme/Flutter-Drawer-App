727efec248d0fc0803699821e8301bd7390910e3
Ayesha Tahir <ayeshatahirme@gmail.com>
2021-04-20 09:00:23 +0500

    added card and alert feature in main.dart 🚀

--- flutter_drawer_app/lib/main.dart
+++ flutter_drawer_app/lib/main.dart
@@ -1,23 +1,120 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MainScreen());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+    debugShowCheckedModeBanner: false,
+	  home: HomePage(),
+	);
+}
 }
 
-class MainScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Scaffold(
+class HomePage extends StatefulWidget {
+@override
+_HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+@override
+Widget build(BuildContext context) {
+
+	return Scaffold(
     appBar: AppBar(
-      title: Text('Email'),
-      centerTitle: true,
-      ),
-      body: Center(
-        child: Text('No messages here!'),
+          title: Text('Flutter Card'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[900],
+          elevation: 5,
+          shadowColor: Colors.blueGrey[300],
         ),
-        drawer: Drawer(
+        body: Center(            
+          child: Card(
+            elevation: 40,
+            shadowColor: Colors.blueGrey[300],
+            color: Colors.white,
+            child: SizedBox(
+              width: 300,
+              height: 350,
+              child: Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children:[
+                    CircleAvatar(
+                      radius: 73.0,
+                      backgroundColor: Colors.blue[900],
+                      child: CircleAvatar(
+                        radius: 70.0,
+                        backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/48019432?v=4'),
+                      ),
+                    ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Text('Scooby Doo',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[900],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                        ),
+                      SizedBox(
+                        width: 90,
+                        child: RaisedButton(
+                          color: Colors.blue[900],
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.details,
+                                color: Colors.white,
+                                ),
+                                Text('More',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  ),
+                                ),
+                              ]
+                            ),
+                            ),
+                            onPressed: (){
+                              return showDialog(
+                                context: context, 
+                                builder: (ctx) => AlertDialog(
+                                  title: Text('Alert',
+                                  style: TextStyle(color: Colors.red),),
+                                  content: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      onPressed: (){
+                                        Navigator.of(ctx).pop();
+                                      }, 
+                                      child: Text('Ok'),
+                                      ),
+                                      FlatButton(
+                                      onPressed: (){
+                                        Navigator.of(ctx).pop();
+                                      }, 
+                                      child: Text('Cancel'),
+                                      )
+                                  ],
+                                )
+                                );
+                          },
+                          ),
+                      ),
+                  ],
+                ),
+                ),
+              )
+            ),
+            
+          ),      
+          drawer: Drawer(
           child: ListView(
             children: <Widget>[
               DrawerHeader(
@@ -30,13 +127,13 @@ class MainScreen extends StatelessWidget {
                     ),
                     Padding(padding: EdgeInsets.all(1.0),
                     ),
-                    Text('Ayesha Tahir',
+                    Text('Scooby Doo',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 16,
                     )
                     ),
-                    Text('ayesha@gmail.com',
+                    Text('scoobydoo@gmail.com',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 14,
@@ -46,7 +143,7 @@ class MainScreen extends StatelessWidget {
                 ),
                 
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: Colors.blue[900],
                 ),
                 
                 ),
@@ -141,9 +238,7 @@ class MainScreen extends StatelessWidget {
 
             ]
           ),
-        ),
-    ),
-  );
+        ),  
+	  );
   }
-  
 }
